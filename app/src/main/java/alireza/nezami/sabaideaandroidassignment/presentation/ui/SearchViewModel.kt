29cc12854c2ba9751ab830b9668c4d7f5e120e45
AlirezaNezami <alireza.nezami75@gmail.com>
2022-07-11 16:17:12 +0430

    Added DI, Network, Coroutines and Coil dependencies.

--- app/src/main/java/alireza/nezami/sabaideaandroidassignment/presentation/ui/SearchViewModel.kt
+++ app/src/main/java/alireza/nezami/sabaideaandroidassignment/presentation/ui/SearchViewModel.kt
@@ -2,9 +2,10 @@ package alireza.nezami.sabaideaandroidassignment.presentation.ui
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class SearchViewModel(
+class SearchViewModel @Inject constructor(
 
 ) :ViewModel(){
 
